3572334b229b67d3be2997e88631ba92660c57e5
ninkira <neckertz921@gmail.com>
2021-10-02 22:32:16 +0200

    Clean up and insert doc in DataSetCreation.py§

--- DataScripts/DataProcessing/DatasetCreation/DataSetCreation.py
+++ DataScripts/DataProcessing/DatasetCreation/DataSetCreation.py
@@ -12,10 +12,14 @@ class DataSet():
         self.dataframe = pd.DataFrame()
 
     def create_labels(self) -> pd.DataFrame:
-        print("create Dataset starts")
+        """
+        Iteriert durch das Rohdatenset und erstellt einen DataFrame mit den Posts und den dazugehörigen Labels.
+        :return: Dataframe mit Labels und Texten.
+        """
+        print("=== Erstelle Datenset ===")
 
         aita_data = ProcessingBase().load_datafile("DataFiles", "posts_json_large.json")
-        print("aita_data", aita_data)
+        print("AITA Rohdaten - Anzahl der Datenpunkte", len(aita_data))
 
         dataset_dataframe = pd.DataFrame()
 
@@ -27,21 +31,16 @@ class DataSet():
         total_count_postings = 0
         official_rating = {}
         label = 0
-        print("aita_data length", len(aita_data))
+
         for batch_idx, post_batch in enumerate(aita_data):
 
-
             for idx, post in enumerate(post_batch):
-                print("idx, post", idx)
                 verdict = post["verdict"]
-                print("verdict", verdict)
                 content = post["content"]
-                if verdict != None and content != "[removed]" and content != "[deleted]":
-                    print("verdict", verdict)
+                if verdict != None and content != "[removed]" and content != "[deleted]": # hier werden Datenpunkte ohne Relevanz aussortiert
                     if verdict == "Not the A-hole":
                         nta_count += 1
                         label = int(0)
-                        print("official nta", nta_count)
                     elif verdict == "Asshole":
                         yta_count += 1
                         label = int(1)
@@ -53,12 +52,10 @@ class DataSet():
                     else:
                         neutral_count += 1
                         label = int(3)
-                    print("label", label)
                     dataset_dataframe = dataset_dataframe.append({
                         "label": label,
                         "text": content
                     }, ignore_index=True)
-                    print(dataset_dataframe)
                     label = 0
                     content = ''
                 else:
@@ -72,71 +69,56 @@ class DataSet():
         official_rating['total_esh'] = esh_count
         official_rating['total_nah'] = nah_count
         official_rating['total_neutral'] = neutral_count
-        print("official rating", official_rating)
-
-        # convert labels to int - otherwise problems later on with BERT
-        # source https://www.linkedin.com/pulse/change-data-type-columns-pandas-mohit-sharma/
+        # Konvertiere Label in int - sonst Probleme mit DistilBERT
         dataset_dataframe = dataset_dataframe.astype({'label': int, 'text': str})
         return dataset_dataframe
 
     def sort_dataframe(self, dataframe: pd.DataFrame):
-        # https://stackoverflow.com/questions/40462675/sort-dataframe-by-first-column-pandas
+        """
+        Sortiert die DataFrame Zeilen nach Spalten, in diesem Fall nach den Werten in der Spalte "Label".
+        :param dataframe: Zu bearbeitender DataFrame
+        """
         dataframe = dataframe.sort_values(dataframe.columns[0], ascending=False)
-        print("sorted", dataframe)
         self.dataframe = dataframe
 
     def create_dataset(self):
-        print("Test test", self.dataframe)
-        # get yta and nta
-        # https://www.kite.com/python/answers/how-to-split-a-pandas-dataframe-into-multiple-dataframes-by-column-value-in-python#:~:text=Splitting%20a%20pandas%20Dataframe%20into,with%20the%20Dataframe%20was%20found.
+        """
+        Erstelle Datensets aus dem zuvor erstellten und sortierten Dataframe. Hier werden alle Datenpunkte mit NTA oder YTA Label bearbeitet für die binäre Klassizifierzung mit DistilBERT.
+        Ziel ist es, Datensets mit gleicher Anzahl an Datenpunkten bereit zu stellen, da weniger YTA-Daten als NTA-Daten vorhanden sind.
+        """
+        # Erstelle Sub-Dataframe mit YTA und NTA Daten.
         grouped = self.dataframe.groupby(self.dataframe.columns[0])
-        print("grouped", grouped)
         group_nta = grouped.get_group(0)  # NTA
         group_yta = grouped.get_group(1)
 
-        # create equal length
-        print("label_nta", group_nta)
+        # Ermittle Größe für die Datensets
         print("len_label_yta", len(group_yta.index))
         print("len_label_nta before", len(group_nta.index))
         yta_size = len(group_yta.index)
-        # https://stackoverflow.com/questions/15943769/how-do-i-get-the-row-count-of-a-pandas-dataframe
-        print("label_yta", group_yta)
         group_nta = group_nta.reset_index()
         group_yta = group_yta.reset_index()
-        # https://pandas.pydata.org/pandas-docs/dev/reference/api/pandas.DataFrame.reset_index.html
+
         for idx, element in group_nta.iterrows():
             print("index", idx)
             if idx >= yta_size:
                 print("Delete row")
-                # https://www.google.com/search?q=remove+rows+from+dataframe&client=opera-gx&hs=UQ4&sxsrf=AOaemvKIzwsninH46-zdimM0bDf9SFl7hA%3A1630960593753&ei=0Xs2Ycq4LaaVxc8Pgua9kAE&oq=remove+rows&gs_lcp=Cgdnd3Mtd2l6EAMYAjIFCAAQywEyBQgAEMsBMgoIABCABBCHAhAUMgUIABDLATIFCAAQywEyBQgAEIAEMgUIABDLATIFCAAQgAQyBQgAEMsBMgUIABDLAToHCCMQsAMQJzoHCAAQRxCwA0oECEEYAFDMWVjeXmDHcGgBcAJ4AIABXogB5AKSAQE0mAEAoAEByAEJwAEB&sclient=gws-wiz
                 group_nta = group_nta.drop(idx)
 
-        print("len_label_nta final", len(group_nta.index))
-        print("len_label_nya final", len(group_yta.index))
+        print("len_label_nta Final", len(group_nta.index))
+        print("len_label_nya Final", len(group_yta.index))
 
         nta_train, nta_test, nta_val = self.sort_into_dataset(group_nta)
-        # print("nta_train", len(nta_train))
-        # print("nta_test",len(nta_test))
-        # print("nta_val", len(nta_val))
 
         yta_train, yta_test, yta_val = self.sort_into_dataset(group_yta)
-        print("yta_train", len(yta_train))
-        print("yta_test", len(yta_test))
-        print("yta_val", len(yta_val))
 
         joined_train = nta_train + yta_train
         joined_test = nta_test + yta_test
         joined_val = nta_val + yta_val
 
         print("joined", len(joined_train), len(joined_test), len(joined_val))
-
-        # final_dataframe = pd.DataFrame()
-        # https://stackoverflow.com/questions/36526282/append-multiple-pandas-data-frames-at-once
-
         train_dataframe = {
             'data': joined_train
         }
-
         test_dataframe = {
             'data': joined_test
         }
@@ -144,8 +126,7 @@ class DataSet():
             'data': joined_val
         }
 
-        print("final dataframe", type(train_dataframe))
-
+       # Speicher in Dateien
         with open(ntpath.join("DataFiles/DataSets/DataSet-L/", 'dataset_l_train.json'), 'w') as fp:
             json.dump(train_dataframe, fp, sort_keys=True, indent=4)
 
@@ -155,24 +136,22 @@ class DataSet():
         with open(ntpath.join("DataFiles/DataSets/DataSet-L/", 'dataset_l_val.json'), 'w') as fp:
             json.dump(val_dataframe, fp, sort_keys=True, indent=4)
 
-
-
     def sort_into_dataset(self, category_dataset):
+        """
+        Sortiere die Datenpunkte in das jeweilige Sub-Datenset. Hier werden für entweder NTA oder YTA jeweils train_data, test_data, validation_data erstellt.
+        :param category_dataset: Die Kategorie für die die Datensets erstellt werden sollen, daher entweder NTA oder YTA.
+        :return: train_data, test_data, validation_data - fertigen Datensets.
+        """
+        # Bestimme Länge der train, test, val Datsets
 
-        # get length for train, test, val
-        print("final dataframe length", len(category_dataset.index))
         train_set_length = len(category_dataset.index) / 100 * 80
-        # runden in python https://qastack.com.de/programming/2356501/how-do-you-round-up-a-number-in-python
         train_set_length = int(math.ceil(train_set_length))
 
-        print("train set length", train_set_length)
         test_set_length = len(category_dataset.index) / 100 * 10
         test_set_length = int(math.ceil(test_set_length))
-        print("test set length", test_set_length)
 
         validation_set_length = len(category_dataset.index) / 100 * 10
         validation_set_length = int(math.ceil(validation_set_length))
-        print("validation set length", validation_set_length)
 
         train_data = []
         tmp_dict = {}
@@ -181,10 +160,9 @@ class DataSet():
         validation_data = []
         val_dict = {}
         stop_num = train_set_length - 1
-        print("stop", stop_num)
         stop_num_2 = train_set_length + test_set_length - 1
-        print("stop2", stop_num_2)
 
+        # Einfügen in die Datensets
         for idx, point in category_dataset.iterrows():
             print("index new", idx)
             if idx <= train_set_length - 1:
@@ -192,7 +170,7 @@ class DataSet():
                 tmp_dict['label'] = point['label']
                 tmp_dict['text'] = point['text']
                 train_data.append(tmp_dict)
-                tmp_dict =  {}
+                tmp_dict = {}
             if idx > stop_num and idx <= stop_num_2:
                 print("fill test data")
                 test_dict['label'] = point['label']
